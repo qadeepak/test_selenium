407bb8365a8fd1991118f5425f7bd94f28785fdd
qadeepak <deepaksavita09@gmail.com>
2017-06-04 18:00:16 +0530

    Demo

--- src/test/java/TestCase/Create_Opertunity.java
+++ src/test/java/TestCase/Create_Opertunity.java
@@ -173,7 +173,7 @@ public class Create_Opertunity {
         
        
         
-        Thread.sleep(4000);
+        Thread.sleep(5000);
         
         Sample01.selectcheckbox(driver).click();
         
